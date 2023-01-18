c3992f44641eaf0fcefe2e4fc339d7ca647d6716
DoxiSvk <91240836+DoxiSvK@users.noreply.github.com>
2023-01-18 22:10:12 +0100

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,47 +1,37 @@
-object Main {
-  def main(args: Array[String]): Unit = {
-    case class Color(red: Int, green: Int, blue: Int)
-    case class Pos(x: Int, y: Int)
+object main extends App {
 
-    sealed trait Pixel {
-      def pos: Pos
-      def color: Color
+  val dimension = 3
+  val screen =
+    """|0:100,125, 56,200
+       |1:255,  0,  0
+       |2:  0,135,200
+       |3:220, 12,  0,100
+       |4: 45, 97,  0
+       |5:  0,  0,  0
+       |6:  0,  0,  0,0
+       |7:  0,  0,  0
+       |8:255,255,255
+       |""".stripMargin
+  case class Pos(y : Int , x : Int)
+  case class Props(red:Int, green:Int, blue:Int, trans: Option[Int])
+  case class Pixel(pos:Pos,props:Props)
+  val pixels: List[Pixel] =
+
+    for (line <- screen.split("\n").toList.map(_.replace(" ",""))) yield {
+      line.trim match{
+        case s"$id:$red,$green,$blue,$trans" =>{
+          Pixel(Pos(id.toInt / dimension , id.toInt % dimension), Props(red.toInt, green.toInt, blue.toInt, trans.toIntOption))}
+        case s"$id:$red,$green,$blue" => {
+          Pixel(Pos(id.toInt / dimension , id.toInt % dimension), Props(red.toInt, green.toInt, blue.toInt, None))}
+      }
     }
-    case class TransparentPixel(pos: Pos, color: Color, transparency: Int) extends Pixel
-    case class RGBPixel(pos: Pos, color: Color) extends Pixel
-
-    val dimension = 3
-    val screen =
-      """|0:100,125, 56,200
-         |1:255,  0,  0
-         |2:  0,135,200
-         |3:220, 12,  0,100
-         |4: 45, 97,  0
-         |5:  0,  0,  0
-         |6:  0,  0,  0,0
-         |7:  0,  0,  0
-         |8:255,255,255
-         |""".stripMargin
-
-    val pixels = screen.stripMargin.split("\n").zipWithIndex.map {
-      case (line, index) =>
-        val Array(x, y) = index.toString.split(":")
-        val Array(r, g, b, t) = line.split(",").map(_.trim)
-        val transparency = t.toInt
-        if (transparency > 0) TransparentPixel(Pos(x.toInt, y.toInt), Color(r.toInt, g.toInt, b.toInt), transparency)
-        else RGBPixel(Pos(x.toInt, y.toInt), Color(r.toInt, g.toInt, b.toInt))
-    }.toList
-
-    val redPixels = pixels.groupBy(_.pos.x).mapValues { row =>
-      row.reduce((p1, p2) => if (p1.color.red > p2.color.red) p1 else p2)
-    }
-
-    val mostTransparentPixel = pixels.filter(_.isInstanceOf[TransparentPixel]).reduce((p1, p2) => {
-      if (p1.asInstanceOf[TransparentPixel].transparency > p2.asInstanceOf[TransparentPixel].transparency) p1 else p2
-    }).pos
-
-    println(pixels)
-    println(redPixels)
-    println(mostTransparentPixel)
+  pixels.foreach(println)
+  val red : Map[Int , Pixel] =  pixels.groupBy(_.pos.y).map{
+    case (riadok,pixel) => (riadok,pixel.maxBy(_.props.red))
   }
+  println("max red --> " + red)
+  val trans : Map[Int,Int] = pixels.groupBy(_.pos.y).map{
+    case (riadok,pixel) => (riadok,(pixel.maxBy(_.props.trans)).pos.x)
+  }
+  println("max trans --> " + trans)
 }
